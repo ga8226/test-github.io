3c156129f3c892aebabb926ce20f6c861bbf910c
ga8226 <132032202+ga8226@users.noreply.github.com>
2023-06-20 12:04:10 +0900

    테스트

---
+++ js/my.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 30,
+    effect: 'fade',
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    autoplay :{
+        delay: 2500,
+
+      }
+  });
+
+
+  
